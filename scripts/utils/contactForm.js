a609f21abe8b1251a8cdb70be1c93be62f5f5cc1
HugoDlhOC <hugo.delahaye53@outlook.fr>
2022-01-07 19:11:51 +0100

    formulaire OK

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,34 @@
+const modal = document.getElementById("contact_modal");
+const modalSend = document.querySelector("#send_form");
+const header = document.querySelector("header");
+const main = document.querySelector("#main");
+const dataInput = document.querySelectorAll("input");
+
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    header.style.display = "none";
+    main.style.display = "none";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    header.style.display = "flex";
+    main.style.display = "block";
+
+
+    //Affichage des données saisies à la console
+    dataInput.forEach((oui) => {
+        console.log(oui.value);
+    });
 }
+
+//Si l'on clique sur le bouton envoyer
+modalSend.addEventListener("click", (e) => {
+    e.preventDefault();
+    closeModal();
+});
+
+
+
